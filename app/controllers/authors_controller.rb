dd1a2bc8ef3822313a075576dc1ae733d96773c6
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-06 01:40:31 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,13 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    if @post.valid? 
     @author = Author.create!(author_params)
 
     redirect_to author_path(@author)
+  else 
+    render :new 
+  end 
   end
 
   private
