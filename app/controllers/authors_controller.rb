ae479baab46900dc71b0268a8b4672437ca14967
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-06 02:05:40 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,8 +9,8 @@ class AuthorsController < ApplicationController
   def create
 
     @author = Author.new(author_params)
-if @post.valid?
-  @post.save
+if @author.valid?
+  @author.save
     redirect_to author_path(@author)
   else
     render :new
